3bfd65293dae5de0c541a37059a53c7d932e2611
yan.liu <yan.liu@sumscope.com>
2016-11-08 17:08:52 +0800

    delete

--- 抽象工厂/抽象工厂1/YaBo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace 抽象工厂1
-{
-    public abstract class YaBo
-    {
-        public abstract void Print();
-    }
-}
--- 抽象工厂/抽象工厂1/YaJia.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace 抽象工厂1
-{
-    public abstract class YaJia
-    {
-        public abstract void Print();
-    }
-}
