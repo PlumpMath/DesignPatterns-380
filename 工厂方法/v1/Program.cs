bd084a8e725f454fb9b75634b0cd8ecb69d113ec
yan.liu <yan.liu@sumscope.com>
2016-11-09 13:52:05 +0800

    工厂方法

---
+++ 工厂方法/v1/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个数字");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("请输入第二个数字");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("请输入运算符号");
+            string op = Console.ReadLine();
+
+            int result = 0;
+
+            Computer com = SimpleFactory.LoadComputer(op);
+
+            com.NumberA = a;
+            com.NumberB = b;
+            result = com.Result;
+
+            Console.WriteLine("{0}{1}{2}={3}", a, op, b, result);
+
+            Console.ReadKey();
+        }
+    }
+
+
+    public static class SimpleFactory
+    {
+        /// <summary>
+        /// 简单工厂方法
+        /// *封装了对象实例创建的复杂度
+        /// </summary>
+        /// <param name="op">用来区分具体产品的标识</param>
+        /// <returns>产品的抽象</returns>
+        public static Computer LoadComputer(string op)
+        {
+            Computer com = null;
+            switch (op)
+            {
+                case "+":
+                    com = new AddComputer();
+                    break;
+                case "-":
+                    com = new JianComputer();
+                    break;
+                case "*":
+                    com = new ChengComputer();
+                    break;
+                case "/":
+                    com = new ChuComputer();
+                    break;
+                default:
+                    com = new AddComputer();
+                    break;
+            }
+            return com;
+        }
+    }
+
+    /// <summary>
+    /// 运算类
+    /// </summary>
+    public abstract class Computer
+    {
+        protected int _NumberA;
+        protected int _NumberB;
+
+        public int NumberA
+        {
+            get { return _NumberA; }
+            set { _NumberA = value; }
+        }
+
+        public int NumberB
+        {
+            get { return _NumberB; }
+            set { _NumberB = value; }
+        }
+
+        /// <summary>
+        /// 子类必须完成的功能：计算并返回结果
+        /// </summary>
+        public abstract int Result { get; }
+    }
+
+    public class AddComputer : Computer
+    {
+        public override int Result
+        {
+            get { return base._NumberA + base._NumberB; }
+        }
+    }
+
+    public class JianComputer : Computer
+    {
+        public override int Result
+        {
+            get { return base._NumberA - base._NumberB; }
+        }
+    }
+    public class ChengComputer : Computer
+    {
+        public override int Result
+        {
+            get { return base._NumberA * base._NumberB; }
+        }
+    }
+    public class ChuComputer : Computer
+    {
+        public override int Result
+        {
+            get
+            {
+                if (base._NumberB == 0)
+                {
+                    throw new ArgumentException("被除数不能为0");
+                }
+                return base._NumberA / base._NumberB;
+            }
+        }
+    }
+}
---
+++ 工厂方法/v2/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个数字");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("请输入第二个数字");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("请输入运算符号");
+            string op = Console.ReadLine();
+
+            int result = 0;
+
+            Computer com = null;
+
+            Factory factory = null;
+
+            switch (op)
+            {
+                case "+":
+                    factory = new FactoryAdd();
+                    break;
+                case "-":
+                    factory = new FactoryJian();
+                    break;
+                case "*":
+                    factory = new FactoryCheng();
+                    break;
+                case "/":
+                    factory = new FactoryChu();
+                    break;
+                default:
+                    factory = new FactoryAdd();
+                    break;
+            }
+
+            com = factory.CreateComputer();
+
+            com.NumberA = a;
+            com.NumberB = b;
+
+            result = com.Result;
+
+            Console.WriteLine("{0}{1}{2}={3}", a, op, b, result);
+
+            Console.ReadKey();
+        }
+    }
+
+
+    public abstract class Factory
+    {
+        public abstract Computer CreateComputer();
+    }
+
+    public class FactoryAdd : Factory
+    {
+        public override Computer CreateComputer()
+        {
+            return new AddComputer();
+        }
+    }
+
+    public class FactoryJian : Factory
+    {
+        public override Computer CreateComputer()
+        {
+            return new JianComputer();
+        }
+    }
+    public class FactoryCheng : Factory
+    {
+        public override Computer CreateComputer()
+        {
+            return new ChengComputer();
+        }
+    }
+    public class FactoryChu : Factory
+    {
+        public override Computer CreateComputer()
+        {
+            return new ChuComputer();
+        }
+    }
+
+    /// <summary>
+    /// 运算类
+    /// </summary>
+    public abstract class Computer
+    {
+        protected int _NumberA;
+        protected int _NumberB;
+
+        public int NumberA
+        {
+            get { return _NumberA; }
+            set { _NumberA = value; }
+        }
+
+        public int NumberB
+        {
+            get { return _NumberB; }
+            set { _NumberB = value; }
+        }
+
+        /// <summary>
+        /// 子类必须完成的功能：计算并返回结果
+        /// </summary>
+        public abstract int Result { get; }
+    }
+
+    public class AddComputer : Computer
+    {
+        public override int Result
+        {
+            get { return base._NumberA + base._NumberB; }
+        }
+    }
+
+    public class JianComputer : Computer
+    {
+        public override int Result
+        {
+            get { return base._NumberA - base._NumberB; }
+        }
+    }
+    public class ChengComputer : Computer
+    {
+        public override int Result
+        {
+            get { return base._NumberA * base._NumberB; }
+        }
+    }
+    public class ChuComputer : Computer
+    {
+        public override int Result
+        {
+            get
+            {
+                if (base._NumberB == 0)
+                {
+                    throw new ArgumentException("被除数不能为0");
+                }
+                return base._NumberA / base._NumberB;
+            }
+        }
+    }
+}
